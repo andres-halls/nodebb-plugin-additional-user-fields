bd1a7aededbcb53074c08aced9370a60c9fbd835
andres-liiver <andres.liiver@gmail.com>
2016-01-21 20:26:47 +0200

    Initial test version

---
+++ library.js
@@ -0,0 +1,13 @@
+(function(module) {
+  'use strict';
+
+  const fields = [''];
+
+  var Plugin = {};
+
+  Plugin.addFields = function(object, callback) {
+    console.log(object);
+  };
+
+  module.exports = Plugin;
+}(module));
